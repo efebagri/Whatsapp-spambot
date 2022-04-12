22865309c6c304d09f7e0ab511bcbbcdd88f6634
TheLinuxRaze <81169584+Razetro@users.noreply.github.com>
2022-04-12 23:43:51 +0200

    Update index.js
    dotenv was added

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const spambot = require("puppeteer");
+require('dotenv').config()
 
 async function scrape(url) {
   const browser = await spambot.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
-  await page.waitForSelector("span [title='YOUR_CONTACT_NAME']");
-  const target = await page.$("span [title='YOUR_CONTACT_NAME']");
+  await page.waitForSelector("span [title='" + process.env.YOUR_CONTACT_NAME + "']");
+  const target = await page.$("span [title='" + process.env.YOUR_CONTACT_NAME + "']");
   await target.click();
   const inp = await page.$(
-    "COPY_SELECTOR"
+    process.env.SELECTOR
   );
 
   for (let i = 0; i < 99999999999; i++) {
-    await inp.type("YOUR_MESSAGE");
+    await inp.type(process.env.YOUR_MESSAGE);
     await page.keyboard.press("Enter");
   }
 }
